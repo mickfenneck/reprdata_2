c65205749d8e20ca5aefca5e53cb30451e1a262e
Michele Sordo <michele.sordo@gmail.com>
2014-05-23 09:53:44 +0200

    damage

--- script.R
+++ script.R
@@ -1,11 +1,9 @@
 data <- read.csv(bzfile("repdata-data-StormData.csv.bz2"))
 str(data)
+
 inj_event <- aggregate(INJURIES ~ EVTYPE, data=data, FUN=sum)
-
-aggregate(maryland$Emissions, by = list(maryland$type, maryland$year), FUN=sum)
-inj <- aggregate(data$INJURIES, by = list(data$EVTYPE, data$))
-
 death_event <- aggregate(FATALITIES ~ EVTYPE, data=data, FUN=sum)
+
 head(inj_event)
 head(death_event)
 max(inj_event$INJURIES)
@@ -21,4 +19,13 @@ most_death$EVTYPE <- as.character(most_death$EVTYPE)
 most_death$EVTYPE <- as.factor(most_death$EVTYPE)
 most_death$FATALITIES <- as.numeric(most_death$FATALITIES)
 
+most_inj$EVTYPE <- as.character(most_inj$EVTYPE)
+most_inj$EVTYPE <- as.factor(most_inj$EVTYPE)
+most_inj$INJURIES <- as.numeric(most_inj$INJURIES)
 
+#seleziono solo quelli con la b e poi raggruppo
+datasub <- data[data$PROPDMGEXP=="B",c("EVTYPE","PROPDMG","PROPDMGEXP")]
+damage_sorted <- datasub[order(datasub$PROPDMG,decreasing=TRUE),]
+#aggrego e ordino
+damage_aggr <- aggregate(PROPDMG ~ EVTYPE, data=damage_sorted,FUN=sum)
+damage_aggr_ord <- damage_aggr[order(damage_aggr$PROPDMG,decreasing=TRUE),]
